c7ae7b7f6f6a42999f5c5d458ce121040802153d
Jonathan Commins <joncom@gmail.com>
2014-03-30 23:38:05 -0700

    Remove network logic.

--- impact/lib/plugins/server.js
+++ impact/lib/plugins/server.js
@@ -14,20 +14,20 @@ ig.module(
         init: function() {
             var self = this;
             ig.io.sockets.on('connection', function(socket) {
+
+                console.log("Client " + socket.id + " connected.");
+
                 socket.on('disconnect', function() {
-                    self.clientDisconnected(this);
-                }).on('reconnect', function() {
-                    self.clientReconnected(this);
-                }).on('screen.move', function(obj) {
-                   this.screen.x = obj.x;
-                   this.screen.y = obj.y;
-                }).on('input.event', function(obj) {
+                    console.log("Client " + socket.id + " disconnected.");
+                })
+
+                .on('reconnect', function() {
+                    console.log("Client " + socket.id + " reconnected.");
+                })
+
+                .on('input.event', function(obj) {
                     socket.input['set_' + obj.type](obj.action);
-                }).on('input.mousemove', function(obj) {
-                    this.input.mouse.x = obj.x;
-                    this.input.mouse.y = obj.y;
-                });
-                self.clientConnected(socket);
+                })
             });
             // Every second calculate the average ping for clients.
             this.clientInterval = setInterval(function() {
@@ -47,48 +47,6 @@ ig.module(
         broadcast: function(key, data) {
             for (var i in this.clients)
                 this.emit(this.clients[i], key, data);
-        },
-        clientConnected: function(socket) {
-            console.log('[INFO] Client connected: ' + socket.id);
-            this.clients[socket.id] = socket;
-            // Each client needs its own input class.
-            socket.input = new ig.Input();
-            socket.screen = { x: 0, y: 0 };
-            ig.latency(socket);
-            // Send the client all the active entities
-            var self = this;
-            ig.game.entities.forEach(function(ent) {
-                self.entityCreate(ent.classType, ent.pos.x, ent.pos.y, ent._settings, socket);
-            });
-            this.broadcast('client.connect', { id: socket.id });
-        },
-        clientReconnected: function(socket) {
-            console.log('[INFO] Client reconnected: ' + socket.id);
-            this.broadcast('client.reconnect', { id: socket.id });
-        },
-        clientDisconnected: function(socket) {
-            console.log('[INFO] Client disconnected: ' + socket.id);
-            this.broadcast('client.disconnect', { id: socket.id });
-            this.clients[socket.id] = undefined;
-            // Remove all entities for the client that disconnected.
-            // Count down so removing entities doesnt mess anything up.
-            var cnt = ig.game.entities.length - 1;
-            var removed = 0;
-            for (var i = cnt; i >= 0; i--) {
-                var ent = ig.game.entities[i];
-                // Use removeEntity instead of kill.
-                if (ent.owner == socket.id) {
-                    ig.game.removeEntity(ig.game.entities[i]);
-                    removed++;
-                }
-            }
-            console.log('[INFO] Removing ' + removed + ' entities');
-        },
-        entityCreate: function(typeStr, x, y, settings, toSocket) {
-            var data = { type: typeStr, x: x, y: y, settings: settings };
-            var key = 'entity.create';
-            if (toSocket) this.emit(toSocket, key, data);
-            else this.broadcast(key, data);
         },
         classToString: function(classObj) {
             // Node has a relatively thin global object so
